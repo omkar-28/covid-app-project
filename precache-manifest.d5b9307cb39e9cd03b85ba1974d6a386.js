6cd6749b57c26a36bf068a7758925610e4bfd517
omkar <omikhatavkar2@gmail.com>
2020-06-02 13:41:15 +0530

    Updates

---
+++ precache-manifest.d5b9307cb39e9cd03b85ba1974d6a386.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0d3cc6744dd12ce4d9d95243af89cfc",
+    "url": "/covid-app-project/index.html"
+  },
+  {
+    "revision": "4b05ef924fbf3a32f45e",
+    "url": "/covid-app-project/static/css/main.7cb667a3.chunk.css"
+  },
+  {
+    "revision": "001d9e7afb001a79a582",
+    "url": "/covid-app-project/static/js/2.eeba56f4.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-app-project/static/js/2.eeba56f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b05ef924fbf3a32f45e",
+    "url": "/covid-app-project/static/js/main.463eea5b.chunk.js"
+  },
+  {
+    "revision": "908470f7962cb89cf729",
+    "url": "/covid-app-project/static/js/runtime-main.d92ed317.js"
+  }
+]);
